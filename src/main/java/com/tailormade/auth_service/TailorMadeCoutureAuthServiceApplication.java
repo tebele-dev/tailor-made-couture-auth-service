16ed508ac9c4635ca95b81338ee2f96be5c22c99
tebele-dev <tebelemakanyane@outlook.com>
2026-01-16 13:54:13 +0200

    Initial commit

---
+++ src/main/java/com/tailormade/auth_service/TailorMadeCoutureAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tailormade.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailorMadeCoutureAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TailorMadeCoutureAuthServiceApplication.class, args);
+	}
+
+}
